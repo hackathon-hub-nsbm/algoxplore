90093738d4bea6687fef1fb7bb3c05a0947d8aed
Danuja <danujadanuja25@gmail.com>
2024-10-27 00:21:59 +0530

    chore: mobile menu padding change

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -79,7 +79,7 @@ export default function Home() {
               </div>
             </div>
 
-            <div className="w-full py-5 lg:hidden">
+            <div className="w-full pt-10 lg:hidden">
               <div className="lg:hidden">
                 <select
                   value={activeTab}
