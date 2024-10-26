e96a2530336015ea008f3c56d82af88f67389fb2
Danuja <danujadanuja25@gmail.com>
2024-10-27 00:13:37 +0530

    chore: mascot size reduce

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -59,7 +59,7 @@ export default function Home() {
       <CustomCursor />
       <BackDrop />
       <main className="main flex items-center justify-center min-h-screen">
-        <div className="terminal crt_effect">
+        <div className="overflow-x-hidden overflow-y-scroll terminal crt_effec">
           <Navbar />
           <div className="flex flex-col justify-between items-center lg:flex-row lg:items-start terminal_data text">
             <MascotDisplay />
