8407800759270d3a2e7de9774e0a04ede5463e4c
Danuja <danujadanuja25@gmail.com>
2024-10-27 12:22:24 +0530

    chore: sponsor section

--- src/components/sponsors.tsx
+++ src/components/sponsors.tsx
@@ -1,17 +1,38 @@
+import Image from "next/image";
 import Art from "./art";
 export default function Sponsors() {
   return (
     <div className="lg:pr-20">
-      <div className="container mx-auto border-cover">
-        <p className="text text-lg mx-10 text-justify mt-20 p-5">
-          AlgoXplore 1.0 is event organized by the []. The event is aimed at
-          providing a platform for students to explore the world of algorithms
-          and data structures. The event will consist of a series of lectures
-          and hands-on sessions on various topics in algorithms and data
-          structures. The event will also include a coding competition where
-          participants can showcase their skills in competitive programming
-          <span className="blinking_underscore"> _</span>
-        </p>
+      <div className="w-full flex flex-col items-center gap-10 justify-center border-cover p-10">
+        <div>
+          <p className="text text-center">Bronze Sponsor</p>
+          <Image
+            className="mt-6"
+            src="/logo_pubudu_constructions.png"
+            alt="Pubudu Constructions"
+            width={300}
+            height={300}
+          />
+        </div>
+        <div>
+          <p className="text text-center">Media Sponsor</p>
+          <div className="flex gap-10">
+            <Image
+              className="mt-6"
+              src="/logo_sunday_morning.png"
+              alt="The Sunday Morning"
+              width={190}
+              height={190}
+            />
+            <Image
+              className="mt-6"
+              src="/logo_aruna.jpg"
+              alt="Aruna"
+              width={190}
+              height={190}
+            />
+          </div>
+        </div>
       </div>
       <Art tab="SPONSORS" />
     </div>
