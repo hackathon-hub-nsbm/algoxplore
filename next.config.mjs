7ec4022364dcdff641cf0891dd58ba75d42c4d7d
Yasiru Dharmathilaka <yasiru.dharmathilaka@gmail.com>
2024-10-27 20:51:51 +0530

    fix: asset prefix

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+// @ts-check
+import { PHASE_DEVELOPMENT_SERVER } from "next/constants";
 
-export default nextConfig;
+export default (phase) => {
+  const isDev = phase === PHASE_DEVELOPMENT_SERVER;
+  /**
+   * @type {import('next').NextConfig}
+   */
+  const nextConfig = {
+    assetPrefix: isDev ? undefined : "https://cdn.mydomain.com",
+  };
+  return nextConfig;
+};
