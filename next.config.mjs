17ebf31c8144196888ad8a35f35f2174e231318c
Yasiru Dharmathilaka <yasiru.dharmathilaka@gmail.com>
2024-10-27 21:01:40 +0530

    fix: asset prefix

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,6 @@
-// @ts-check
-import { PHASE_DEVELOPMENT_SERVER } from "next/constants";
-
-export default (phase) => {
-  const isDev = phase === PHASE_DEVELOPMENT_SERVER;
-  /**
-   * @type {import('next').NextConfig}
-   */
-  const nextConfig = {
-    assetPrefix: isDev
-      ? undefined
-      : "https://hackathon-hub-nsbm.github.io/algoxplore",
-  };
-  return nextConfig;
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  assetPrefix: "https://hackathon-hub-nsbm.github.io/algoxplore",
 };
+
+export default nextConfig;
