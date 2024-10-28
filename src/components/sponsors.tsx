5ff48ed782963c695e2446ad20b14670f309a901
ireallywantthatname <rladsilva@students.nsbm.ac.lk>
2024-10-28 08:19:05 +0530

    refactor(sponsors): add new sponsors and arrange logos side by side

--- src/components/sponsors.tsx
+++ src/components/sponsors.tsx
@@ -1,3 +1,5 @@
+import logoGreenBlack from "@/assets/green-black.png";
+import logoHack from "@/assets/hacksl.jpg";
 import logoSunday2 from "@/assets/logo_aruna.jpg";
 import logoPubudu from "@/assets/logo_pubudu_constructions.png";
 import logoSunday1 from "@/assets/logo_sunday_morning.png";
@@ -7,19 +9,41 @@ export default function Sponsors() {
   return (
     <div className="lg:pr-20">
       <div className="w-full flex flex-col items-center gap-10 justify-center border-cover p-10">
-        <div>
-          <p className="text text-center">Bronze Sponsor</p>
-          <Image
-            className="mt-6"
-            src={logoPubudu}
-            alt="Pubudu Constructions"
-            width={300}
-            height={300}
-          />
+        <div className="flex flex-wrap gap-10 justify-center">
+          <div className="flex flex-col items-center">
+            <p className="text text-center">Bronze Sponsor</p>
+            <Image
+              className="mt-6"
+              src={logoPubudu}
+              alt="Pubudu Constructions"
+              width={300}
+              height={300}
+            />
+          </div>
+          <div className="flex flex-col items-center">
+            <p className="text text-center">Sponsored by</p>
+            <Image
+              className="mt-6"
+              src={logoGreenBlack}
+              alt="Code Filine"
+              width={300}
+              height={300}
+            />
+          </div>
+          <div className="flex flex-col items-center">
+            <p className="text text-center">Sponsored by</p>
+            <Image
+              className="mt-6"
+              src={logoHack}
+              alt="Hack SL"
+              width={200}
+              height={200}
+            />
+          </div>
         </div>
         <div>
           <p className="text text-center">Media Sponsor</p>
-          <div className="flex gap-10">
+          <div className="flex gap-10 justify-center">
             <Image
               className="mt-6"
               src={logoSunday1}
