e80402e13be94efec73e319745f7a691214fd16e
ireallywantthatname <rladsilva@students.nsbm.ac.lk>
2024-11-03 06:23:44 +0530

    feat: adds links to the sponsor logos

--- src/components/sponsors.tsx
+++ src/components/sponsors.tsx
@@ -9,6 +9,7 @@ import logoPubudu from "@/assets/logo_pubudu_constructions.png";
 import logoSunday1 from "@/assets/logo_sunday_morning.png";
 import virakesariLogo from "@/assets/virakesariLogo.jpg";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Sponsors() {
   return (
@@ -17,111 +18,131 @@ export default function Sponsors() {
         <div className="flex flex-wrap gap-10 justify-center">
           <div className="flex flex-col items-center">
             <p className="text text-center">Official Bronze Partner</p>
-            <Image
-              className="mt-6"
-              src={logoPubudu}
-              alt="Pubudu Constructions"
-              width={300}
-              height={300}
-            />
+            <Link href="https://web.facebook.com/pubuduconstructions/">
+              <Image
+                className="mt-6"
+                src={logoPubudu}
+                alt="Pubudu Constructions"
+                width={300}
+                height={300}
+              />
+            </Link>
           </div>
           <div className="flex flex-col items-center">
             <p className="text text-center">Official Bronze Partner</p>
-            <Image
-              className="mt-6"
-              src={logoictFromAbc}
-              alt="ICT From ABC"
-              width={300}
-              height={300}
-            />
+            <Link href="https://ictfromabc.com/">
+              <Image
+                className="mt-6"
+                src={logoictFromAbc}
+                alt="ICT From ABC"
+                width={300}
+                height={300}
+              />
+            </Link>
           </div>
         </div>
 
         <div className="flex gap-10 justify-center">
           <div className="flex flex-col items-center">
             <p className="text text-center">Official Bronze Partner</p>
-            <Image
-              className="mt-6"
-              src={logoBrandView}
-              alt="Code Filine"
-              width={150}
-              height={150}
-            />
+            <Link href="https://www.brandviewlabels.com/">
+              <Image
+                className="mt-6"
+                src={logoBrandView}
+                alt="Code Filine"
+                width={150}
+                height={150}
+              />
+            </Link>
           </div>
           <div className="flex flex-col items-center">
             <p className="text text-center">Official Digital Media Partner</p>
-            <Image
-              className="mt-6"
-              src={logoHack}
-              alt="Hack SL"
-              width={150}
-              height={150}
-            />
+            <Link href="https://web.facebook.com/hacksl.tech/">
+              <Image
+                className="mt-6"
+                src={logoHack}
+                alt="Hack SL"
+                width={150}
+                height={150}
+              />
+            </Link>
           </div>
         </div>
 
         <div className="flex flex-col items-center">
           <p className="text text-center">Print and Online Media Partner</p>
-          <Image
-            className="mt-6"
-            src={virakesariLogo}
-            alt="Virakesari"
-            width={300}
-            height={300}
-          />
+          <Link href="https://www.virakesari.lk/">
+            <Image
+              className="mt-6"
+              src={virakesariLogo}
+              alt="Virakesari"
+              width={300}
+              height={300}
+            />
+          </Link>
         </div>
 
         <div>
           <p className="text text-center">Official Media Partner</p>
           <div className="flex gap-10 justify-center">
-            <Image
-              className="mt-6"
-              src={logoSunday1}
-              alt="The Sunday Morning"
-              width={190}
-              height={190}
-            />
-            <Image
-              className="mt-6"
-              src={logoSunday2}
-              alt="Aruna"
-              width={190}
-              height={190}
-            />
+            <Link href="https://www.aruna.lk/">
+              <Image
+                className="mt-6"
+                src={logoSunday1}
+                alt="The Sunday Morning"
+                width={190}
+                height={190}
+              />
+            </Link>
+            <Link href="https://www.aruna.lk/">
+              <Image
+                className="mt-6"
+                src={logoSunday2}
+                alt="Aruna"
+                width={190}
+                height={190}
+              />
+            </Link>
           </div>
         </div>
 
         <div className="flex flex-col items-center">
           <p className="text text-center">Official Gift Partner</p>
-          <Image
-            className="mt-6"
-            src={logoApiFlora}
-            alt="Api Flora"
-            width={200}
-            height={200}
-          />
+          <Link href="https://apiflora.lk/">
+            <Image
+              className="mt-6"
+              src={logoApiFlora}
+              alt="Api Flora"
+              width={200}
+              height={200}
+            />
+          </Link>
         </div>
 
         <div className="flex gap-10 justify-center">
           <div className="flex flex-col items-center">
             <p className="text text-center">Official Media Partner</p>
-            <Image
-              className="mt-6"
-              src={logoIsurumaan}
-              alt="Isurumaan Photography"
-              width={300}
-              height={300}
-            />
+            <Link href="https://isurumaan.com/">
+              <Image
+                className="mt-6"
+                src={logoIsurumaan}
+                alt="Isurumaan Photography"
+                width={300}
+                height={300}
+              />
+            </Link>
           </div>
           <div className="flex flex-col items-center">
             <p className="text text-center">Official Gift Partner</p>
-            <Image
-              className="mt-6"
-              src={logoCodeFiline}
-              alt="Code Filine"
-              width={300}
-              height={300}
-            />
+            <Link href="https://www.codefiline.com/">
+              <Image
+                className="mt-6"
+                src={logoCodeFiline}
+                alt="Code Filine"
+                width={300}
+                height={300}
+              />
+            </Link>
           </div>
         </div>
       </div>
