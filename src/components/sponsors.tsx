32ceddb40fbb843469cd717b4b411d5e761c96df
Akash De Silva <rladsilva@students.nsbm.ac.lk>
2024-11-03 16:18:29 +0530

    feat: adds more partners to sponsors page (#46)
    * refactor(sponsors): add new sponsors and arrange logos side by side
    
    * refactor(sponsors): add new sponsors and arrange logos side by side
    
    * refactor(sponsors): adds more sponsor logos
    
    * feat(FAQ): implements FAQ section
    
    * refactor: adds even more sponsors
    
    * feat: adds links to the sponsor logos
    
    * refactor(sponsors): resizes some logos
    
    * refactor(sponsors): better contrast on media partner logo
    
    * Merge branch 'development' of https://github.com/hackathon-hub-nsbm/algoxplore into refactor/final-sponsors
    
    * fix: removes unused imports
    
    ---------
    
    Co-authored-by: Yasiru Dharmathilaka <yasiru.dharmathilaka@gmail.com>

--- src/components/sponsors.tsx
+++ src/components/sponsors.tsx
@@ -1,12 +1,15 @@
-import apiFloraLogo from "@/assets/api-flora.png";
 import logoBrandView from "@/assets/brandViewLogo.jpg";
-import logoGreenBlack from "@/assets/green-black.png";
 import logoHack from "@/assets/hacksl.jpg";
+import logoApiFlora from "@/assets/logo_api_flora.png";
 import logoSunday2 from "@/assets/logo_aruna.jpg";
+import logoCodeFiline from "@/assets/logo_code_filine.png";
+import logoictFromAbc from "@/assets/logo_ictFromAbc.png";
+import logoIsurumaan from "@/assets/logo_isurumaan.png";
 import logoPubudu from "@/assets/logo_pubudu_constructions.png";
 import logoSunday1 from "@/assets/logo_sunday_morning.png";
 import virakesariLogo from "@/assets/virakesariLogo.jpg";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Sponsors() {
   return (
@@ -15,79 +18,61 @@ export default function Sponsors() {
         <div className="flex flex-wrap gap-10 justify-center">
           <div className="flex flex-col items-center">
             <p className="text text-center">Official Bronze Partner</p>
-            <Image
-              className="mt-6"
-              src={logoPubudu}
-              alt="Pubudu Constructions"
-              width={300}
-              height={300}
-            />
+            <Link href="https://web.facebook.com/pubuduconstructions/">
+              <Image
+                className="mt-6"
+                src={logoPubudu}
+                alt="Pubudu Constructions"
+                width={300}
+                height={300}
+              />
+            </Link>
           </div>
 
           <div className="flex flex-col items-center">
             <p className="text text-center">Official Bronze Partner</p>
-            <Image
-              className="mt-6"
-              src={logoBrandView}
-              alt="Code Filine"
-              width={200}
-              height={200}
-            />
-          </div>
-
-          <div className="flex flex-col items-center">
-            <p className="text text-center">Official Gift Partner</p>
-            <Image
-              className="mt-6"
-              src={logoGreenBlack}
-              alt="Code Filine"
-              width={300}
-              height={300}
-            />
-          </div>
-
-          <div className="flex flex-col items-center">
-            <p className="text text-center">Official Gift Partner</p>
-            <Image
-              className="mt-6"
-              src={apiFloraLogo}
-              alt="Api Flora"
-              width={200}
-              height={200}
-            />
-          </div>
-
-          <div className="flex flex-col items-center">
-            <p className="text text-center">Official Digital Media Partner</p>
-            <Image
-              className="mt-6"
-              src={logoHack}
-              alt="Hack SL"
-              width={200}
-              height={200}
-            />
+            <Link href="https://ictfromabc.com/">
+              <Image
+                className="mt-6"
+                src={logoictFromAbc}
+                alt="ICT From ABC"
+                width={300}
+                height={300}
+              />
+            </Link>
           </div>
         </div>
 
-        <div>
-          <p className="text text-center">Printed Media Partner(s)</p>
-          <div className="flex gap-10 justify-center">
-            <Image
-              className="mt-6"
-              src={logoSunday1}
-              alt="The Sunday Morning"
-              width={190}
-              height={190}
-            />
+        <div className="flex gap-10 justify-center">
+          <div className="flex flex-col items-center">
+            <p className="text text-center">Official Bronze Partner</p>
+            <Link href="https://www.brandviewlabels.com/">
+              <Image
+                className="mt-6"
+                src={logoBrandView}
+                alt="Code Filine"
+                width={150}
+                height={150}
+              />
+            </Link>
+          </div>
+          <div className="flex flex-col items-center">
+            <p className="text text-center">Official Digital Media Partner</p>
+            <Link href="https://web.facebook.com/hacksl.tech/">
+              <Image
+                className="mt-6"
+                src={logoHack}
+                alt="Hack SL"
+                width={150}
+                height={150}
+              />
+            </Link>
+          </div>
+        </div>
 
-            <Image
-              className="mt-6"
-              src={logoSunday2}
-              alt="Aruna"
-              width={190}
-              height={190}
-            />
-
+        <div className="flex flex-col items-center">
+          <p className="text text-center">Print and Online Media Partner</p>
+          <Link href="https://www.virakesari.lk/">
             <Image
               className="mt-6"
               src={virakesariLogo}
@@ -95,6 +80,70 @@ export default function Sponsors() {
               width={300}
               height={300}
             />
+          </Link>
+        </div>
+
+        <div>
+          <p className="text text-center">Official Media Partner</p>
+          <div className="flex gap-10 justify-center">
+            <Link href="https://www.aruna.lk/">
+              <Image
+                className="mt-6"
+                src={logoSunday1}
+                alt="The Sunday Morning"
+                width={270}
+                height={270}
+              />
+            </Link>
+            <Link href="https://www.aruna.lk/">
+              <Image
+                className="mt-6"
+                src={logoSunday2}
+                alt="Aruna"
+                width={190}
+                height={190}
+              />
+            </Link>
+          </div>
+        </div>
+
+        <div className="flex flex-col items-center">
+          <p className="text text-center">Official Gift Partner</p>
+          <Link href="https://apiflora.lk/">
+            <Image
+              className="mt-6"
+              src={logoApiFlora}
+              alt="Api Flora"
+              width={200}
+              height={200}
+            />
+          </Link>
+        </div>
+
+        <div className="flex gap-10 justify-center">
+          <div className="flex flex-col items-center">
+            <p className="text text-center">Official Media Partner</p>
+            <Link href="https://isurumaan.com/">
+              <Image
+                className="mt-6 bg-white"
+                src={logoIsurumaan}
+                alt="Isurumaan Photography"
+                width={300}
+                height={300}
+              />
+            </Link>
+          </div>
+          <div className="flex flex-col items-center">
+            <p className="text text-center">Official Gift Partner</p>
+            <Link href="https://www.codefiline.com/">
+              <Image
+                className="mt-6 bg-white px-3 py-1"
+                src={logoCodeFiline}
+                alt="Code Filine"
+                width={300}
+                height={300}
+              />
+            </Link>
           </div>
         </div>
       </div>
