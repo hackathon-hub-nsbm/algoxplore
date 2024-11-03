72b9cc699210c5039b7e7e450424371879250bee
ireallywantthatname <rladsilva@students.nsbm.ac.lk>
2024-11-03 06:08:06 +0530

    refactor: adds even more sponsors

--- src/components/sponsors.tsx
+++ src/components/sponsors.tsx
@@ -1,7 +1,10 @@
 import logoBrandView from "@/assets/brandViewLogo.jpg";
-import logoGreenBlack from "@/assets/green-black.png";
 import logoHack from "@/assets/hacksl.jpg";
+import logoApiFlora from "@/assets/logo_api_flora.png";
 import logoSunday2 from "@/assets/logo_aruna.jpg";
+import logoCodeFiline from "@/assets/logo_code_filine.png";
+import logoictFromAbc from "@/assets/logo_ictFromAbc.png";
+import logoIsurumaan from "@/assets/logo_isurumaan.png";
 import logoPubudu from "@/assets/logo_pubudu_constructions.png";
 import logoSunday1 from "@/assets/logo_sunday_morning.png";
 import virakesariLogo from "@/assets/virakesariLogo.jpg";
@@ -23,13 +26,26 @@ export default function Sponsors() {
             />
           </div>
           <div className="flex flex-col items-center">
-            <p className="text text-center">Official Gift Partner</p>
+            <p className="text text-center">Official Bronze Partner</p>
             <Image
               className="mt-6"
-              src={logoGreenBlack}
-              alt="Code Filine"
+              src={logoictFromAbc}
+              alt="ICT From ABC"
               width={300}
               height={300}
+            />
+          </div>
+        </div>
+
+        <div className="flex gap-10 justify-center">
+          <div className="flex flex-col items-center">
+            <p className="text text-center">Official Bronze Partner</p>
+            <Image
+              className="mt-6"
+              src={logoBrandView}
+              alt="Code Filine"
+              width={150}
+              height={150}
             />
           </div>
           <div className="flex flex-col items-center">
@@ -38,31 +54,23 @@ export default function Sponsors() {
               className="mt-6"
               src={logoHack}
               alt="Hack SL"
-              width={200}
-              height={200}
-            />
-          </div>
-          <div className="flex flex-col items-center">
-            <p className="text text-center">Print and Online Media Partner</p>
-            <Image
-              className="mt-6"
-              src={virakesariLogo}
-              alt="Virakesari"
-              width={300}
-              height={300}
-            />
-          </div>
-          <div className="flex flex-col items-center">
-            <p className="text text-center">Official Bronze Partner</p>
-            <Image
-              className="mt-6"
-              src={logoBrandView}
-              alt="Code Filine"
-              width={200}
-              height={200}
+              width={150}
+              height={150}
             />
           </div>
         </div>
+
+        <div className="flex flex-col items-center">
+          <p className="text text-center">Print and Online Media Partner</p>
+          <Image
+            className="mt-6"
+            src={virakesariLogo}
+            alt="Virakesari"
+            width={300}
+            height={300}
+          />
+        </div>
+
         <div>
           <p className="text text-center">Official Media Partner</p>
           <div className="flex gap-10 justify-center">
@@ -79,6 +87,40 @@ export default function Sponsors() {
               alt="Aruna"
               width={190}
               height={190}
+            />
+          </div>
+        </div>
+
+        <div className="flex flex-col items-center">
+          <p className="text text-center">Official Gift Partner</p>
+          <Image
+            className="mt-6"
+            src={logoApiFlora}
+            alt="Api Flora"
+            width={200}
+            height={200}
+          />
+        </div>
+
+        <div className="flex gap-10 justify-center">
+          <div className="flex flex-col items-center">
+            <p className="text text-center">Official Media Partner</p>
+            <Image
+              className="mt-6"
+              src={logoIsurumaan}
+              alt="Isurumaan Photography"
+              width={300}
+              height={300}
+            />
+          </div>
+          <div className="flex flex-col items-center">
+            <p className="text text-center">Official Gift Partner</p>
+            <Image
+              className="mt-6"
+              src={logoCodeFiline}
+              alt="Code Filine"
+              width={300}
+              height={300}
             />
           </div>
         </div>
